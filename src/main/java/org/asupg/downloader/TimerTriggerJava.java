7e9fc12391be63dfe056c015077212d48d68a54b
Kamron <shuhikjuriknarik@gmail.com>
2025-12-31 11:44:49 +0500

    Initial commit

---
+++ src/main/java/org/asupg/downloader/TimerTriggerJava.java
@@ -0,0 +1,21 @@
+package org.asupg.downloader;
+
+import java.time.*;
+import com.microsoft.azure.functions.annotation.*;
+import com.microsoft.azure.functions.*;
+
+/**
+ * Azure Functions with Timer trigger.
+ */
+public class TimerTriggerJava {
+    /**
+     * This function will be invoked periodically according to the specified schedule.
+     */
+    @FunctionName("TimerTriggerJava")
+    public void run(
+        @TimerTrigger(name = "timerInfo", schedule = "0 */10 * * * *") String timerInfo,
+        final ExecutionContext context
+    ) {
+        context.getLogger().info("Java Timer trigger function executed at: " + LocalDateTime.now());
+    }
+}
